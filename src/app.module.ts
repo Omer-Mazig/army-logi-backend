0bfa664753f5e1adc2241ed875422e66552599c9
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2025-07-29 23:20:11 +0300

    add cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SoldiersModule } from './soldiers/soldiers.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { SoldiersModule } from './soldiers/soldiers.module';
       envFilePath: '.env',
     }),
     SoldiersModule,
+    ReportsModule,
   ],
   controllers: [],
   providers: [],
--- src/soldiers/soldiers.service.ts
+++ src/soldiers/soldiers.service.ts
@@ -1,23 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, tap, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class SoldiersService {
-  // TODO: move to env
   private readonly url = process.env.GOOGLE_SCRIPT_URL || '';
+  // TODO: fix any
+  private soldiers: any = null; // in memory cache
+  private isUpdating = false;
+
   constructor(private readonly httpService: HttpService) {}
 
   getSoldiers(): Observable<any> {
-    return this.httpService
-      .get(this.url)
-      .pipe(map((response) => response.data));
+    // If we have cached soldiers, return them and update in background
+    if (this.soldiers) {
+      this.updateSoldiersAsync();
+      return of(this.soldiers);
+    }
+
+    // No cache, fetch for the first time
+    return this.httpService.get(this.url).pipe(
+      map((response) => response.data),
+      tap((data) => {
+        this.soldiers = data;
+      }),
+    );
   }
 
   getPersonalNumbers(): Observable<any> {
     return this.httpService
       .get(this.url + '?action=personal-numbers')
       .pipe(map((response) => response.data));
+  }
+
+  private updateSoldiersAsync(): void {
+    // Don't update if already updating
+    if (this.isUpdating) return;
+
+    this.isUpdating = true;
+    this.httpService
+      .get(this.url)
+      .pipe(
+        map((response) => response.data),
+        tap((data) => {
+          this.soldiers = data;
+        }),
+        catchError((error) => {
+          console.error('Failed to update soldiers cache:', error);
+          return of(null);
+        }),
+      )
+      .subscribe(() => {
+        this.isUpdating = false;
+      });
   }
 }
